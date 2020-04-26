72ca6d3fbd93f4c72d96f9b34a3c608b91a2ac91
DMorton <davidmortonmail@gmail.com>
2020-04-26 10:57:56 +0100

    fix(*): can update island data now

--- src/routes/visitor-center/visitor-center.js
+++ src/routes/visitor-center/visitor-center.js
@@ -14,6 +14,7 @@ const VisitorCenter = () => {
     dodoCode,
     isOwner,
     fetchDodoCode,
+    saveCenterData,
   } = useVisitorCenter(centerId);
   const { uid } = useUser();
   const [isEditable, setIsEditable] = useState(false);
@@ -50,6 +51,7 @@ const VisitorCenter = () => {
 
   const updateCenterInformation = () => {
     setIsEditable(!isEditable);
+    if (isEditable) saveCenterData(centerId, centerInformation);
   };
 
   const [latestDodoCode, setDodoCode] = useState({ dodoCode: "*****" });
