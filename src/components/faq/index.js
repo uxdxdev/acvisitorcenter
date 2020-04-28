8b4b4d2a7d158a94f41f27feeddaaf9651bce738
DMorton <dmorton@ie.ibm.com>
2020-04-28 20:47:59 +0100

    fix(*): added faq section

---
+++ src/components/faq/index.js
@@ -0,0 +1,2 @@
+import Faq from "./faq";
+export { Faq };
--- src/routes/landing-page/landing-page.js
+++ src/routes/landing-page/landing-page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { CreateVisitorCenter } from "../../components/create-visitor-center";
 import { LatestVisitorCenterList } from "../../components/latest-visitor-center-list";
+import { Faq } from "../../components/faq";
 
 const LandingPage = () => {
   return (
     <>
       <CreateVisitorCenter />
+      <Faq />
       <LatestVisitorCenterList />
     </>
   );
