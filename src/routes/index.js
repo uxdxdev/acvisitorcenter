719f7c3276cc9e931a5f5801150bd686e9b5c988
DMorton <davidmortonmail@gmail.com>
2020-04-28 09:52:29 +0100

    fix(*): started adding material ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { LandingPage } from "./landing-page";
 import { VisitorCenter } from "./visitor-center";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Routes = () => {
+  const isDevEnv = process.env.NODE_ENV === "development";
+  const [isVerified, setIsVerified] = useState(isDevEnv);
+
+  // recaptcha onChange
+  const onChange = (token) => {
+    if (token) {
+      setIsVerified(true);
+    } else {
+      // token expired
+      setIsVerified(false);
+    }
+  };
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path="/center/:id">
-          <VisitorCenter />
-        </Route>
-      </Switch>
+      <Link to={`/`}>
+        <h1>AC Visitor Center</h1>
+      </Link>
+      {isVerified ? (
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/center/:id">
+            <VisitorCenter />
+          </Route>
+        </Switch>
+      ) : (
+        <ReCAPTCHA
+          sitekey={"6LdRQe4UAAAAACXdpngpIJRqwXsBweATo0zFa-WJ"}
+          onChange={onChange}
+          onExpired={onChange}
+          onErrored={onChange}
+        />
+      )}
     </Router>
   );
 };
