981951d6545bf6aa8be6e62f0283dbba82ab6e2f
DMorton <dmorton@ie.ibm.com>
2020-05-02 13:59:57 +0100

    fix(*): updated faq

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -18,7 +18,7 @@ const steps = [
     body: (
       <>
         Set a Name and Summary for your visitor center, setting a Code is
-        optional and can be updated later, click "Create visitor center", then
+        optional and can be updated later. Click "Create visitor center", then
         open your visitor center page.
       </>
     ),
@@ -28,9 +28,8 @@ const steps = [
     body: (
       <>
         On your visitor center page update your Code by clicking "Edit" in the
-        Code section. Then click "Open gates" to let visitors join the waiting
-        list. You can now "Share" the link to your visitor center with your
-        friends!.
+        Code section. Click "Open gates" to let visitors join the waiting list.
+        You can now "Share" the link to your visitor center with your friends!.
       </>
     ),
   },
@@ -39,8 +38,8 @@ const steps = [
     body: (
       <>
         Visitors who are first in the queue will get a notification prompting
-        them to get the latest code. Once they have the code they can be removed
-        from the queue by clicking "Done".
+        them to get the latest code. Once they arrive they can be removed from
+        the queue by clicking "Done".
       </>
     ),
   },
@@ -53,14 +52,14 @@ const questionsAnswers = [
       "Yes, visitors that are first in the queue will receive a notification to get the latest code. They can use this code until you change it. Removing them from the queue will not prevent them from using the code.",
   },
   {
-    title: "How do I remove/ban people from my visitor center?",
+    title: "How do I ban people from my visitor center?",
     body:
-      "You cannot remove or ban people from the visitor center. You must change your code after removing them from the queue.",
+      "You cannot ban people from the visitor center. However, you could change your code before they move into the 1st, or 'Next', position in the queue, then update your visitor center page with the new code once they are no longer in the waiting list.",
   },
   {
     title: "Can people in the waiting list see my code?",
     body:
-      "No, the code is only available to the first person in the waiting list. They must click the 'Get code' button to see it.",
+      "Only the first visitor in the queue can get your code. They must click the 'Get code' button to see it.",
   },
   {
     title: "How is the waiting list updated?",
@@ -73,9 +72,14 @@ const questionsAnswers = [
       "No, each user can only create a single visitor center. It is easy to update your visitor center by clicking the 'Edit' button next to each label on the visitor center page.",
   },
   {
-    title: "What does 'Visitor center gates are closed' mean?",
+    title: "What does 'Visitor center is closed' mean?",
     body:
-      "It means the owner of the visitor center has either closed the visitor center page or has closed the gates to the visitor center to prevent people joining the queue. The visitor center may still be open if the gates are closed, so stay in the queue!",
+      "It means the owner of the visitor center no longer has the visitor center page open in their browser. The visitor center will not operate correctly if the visitor center page is not open in their browser.",
+  },
+  {
+    title: "What does 'Queue locked' mean?",
+    body:
+      "The owner of the visitor center has locked the queue to prevent more visitors joining the waiting list. Visitors already in the waiting list can still get the code and visit provided the visitor center is still open and the owner is managing the queue.",
   },
 ];
 
