b4ba8bf64c5ebe66ff78c70a5fddbccac4c7d099
DMorton <davidmortonmail@gmail.com>
2020-05-03 16:46:02 +0100

    fix(*): refactoring

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -4,14 +4,14 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/analytics";
 
+// eslint-disable-next-line no-restricted-globals
+const isDevEnv = location.hostname === "localhost";
 const firebaseConfig = {
   apiKey: "AIzaSyDO7BqcLUNL0RCvhlNdO3YGiekQXE9-S-w",
   authDomain: "acvisitorcenter.firebaseapp.com",
-  databaseURL:
-    // eslint-disable-next-line no-restricted-globals
-    location.hostname === "localhost"
-      ? "http://localhost:9000?ns=acvisitorcenter"
-      : "https://acvisitorcenter.firebaseio.com",
+  databaseURL: isDevEnv
+    ? "http://localhost:9000?ns=acvisitorcenter"
+    : "https://acvisitorcenter.firebaseio.com",
   projectId: "acvisitorcenter",
   storageBucket: "acvisitorcenter.appspot.com",
   messagingSenderId: "783477777649",
@@ -25,10 +25,7 @@ const firestoreDb = firebaseApp.firestore();
 // this will connect the browser to the local emulator for firestore, avoiding using the quota during development.
 // IMPORTANT: when viewing the dev version of the app from a remote device using this dev machines IP, the app will
 // connect to the live database.
-if (
-  // eslint-disable-next-line no-restricted-globals
-  location.hostname === "localhost"
-) {
+if (isDevEnv) {
   // Note that the Firebase Web SDK must connect to the WebChannel port
   firestoreDb.settings({
     host: "localhost:8080",
